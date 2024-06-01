c7034e339e51809659b17d2c9672531d61b7a851
papa.black <iam@papa.black>
2024-06-01 04:08:04 +0200

    failed ms build

--- webui-user.bat
+++ webui-user.bat
@@ -1,10 +1,116 @@
-@echo off
 
+
+set PYTORCH_VERSION=2.3.0
+set TORCHVISION_VERSION=0.18.0
 set PYTHON=
 set GIT=
 set VENV_DIR=
 set COMMANDLINE_ARGS=
 set TORCH_INDEX_URL="https://download.pytorch.org/whl/cu121"
-set TORCH_COMMAND="pip install torch==2.2.2 torchvision==0.17.2 --extra-index-url https://download.pytorch.org/whl/cu121"
+set TORCH_COMMAND="pip install torch==%PYTORCH_VERSION% torchvision==%TORCHVISION_VERSION% --extra-index-url https://download.pytorch.org/whl/cu121"
+set MSBUILD_PATH="C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\BuildTools\\VC\\Auxiliary\\Build\\\vcvars64.bat"
+set XFORMERS_PACKAGE="xformers"
 
-call webui.bat --skip-python-version-check --share --
+set GRADIO_USER=""
+set GRADIO_PASS=""
+
+:: Define the packages to install
+set check_choco_packages="mingw llvm"
+
+pip install torch==%PYTORCH_VERSION% torchvision==%TORCHVISION_VERSION% --extra-index-url https://download.pytorch.org/whl/cu121
+
+:: Check if GRADIO_USER is set
+if "%GRADIO_USER%"=="" (
+    echo "GRADIO_USER is not set."
+    set auth=""
+) else (
+    :: Check if GRADIO_PASS is set
+    if "%GRADIO_PASS%"=="" (
+        echo GRADIO_PASS is not set.
+        set auth=""
+    ) else (
+        :: Construct the auth variable
+        set auth="--gradio-auth=%GRADIO_USER%:%GRADIO_PASS%"
+    )
+)
+
+:: Check if Chocolatey is installed
+where choco >nul 2>nul
+if %errorlevel% neq 0 (
+    echo Chocolatey is not installed. Please install Chocolatey and try again.
+    echo "https://chocolatey.org/install"
+    exit /b 1
+)
+
+:: Loop through each package
+for %%p in (%check_choco_packages%) do (
+    :: Check if the package is already installed
+    choco list --local-only %%p >nul 2>nul
+    if %errorlevel% equ 0 (
+        echo Package %%p is already installed.
+    ) else (
+        :: Install the package silently
+        echo Installing package %%p...
+        choco install %%p -y
+        if %errorlevel% neq 0 (
+            echo Failed to install package %%p.
+        )
+
+        echo Installation completed.
+    )
+)
+
+python -c "import wheel; print(wheel.__version__)" 2>nul
+if %ERRORLEVEL% neq 0 (
+    echo "Installing Wheels..."
+    pip install wheel
+)
+
+cd repositories
+
+if not exist "xformers" (
+    echo "Installing XFormers..."
+
+    git clone --recurse-submodules https://github.com/facebookresearch/xformers.git
+)
+
+python -c "import xformers; print(xformers.__version__)" 2>nul
+if %ERRORLEVEL% neq 0 (        
+    cd xformers
+
+    if not exist dist\xformers-*.whl (
+        echo Building XFormers...
+        call %MSBUILD_PATH%
+
+        python setup.py sdist bdist_wheel
+    ) else (
+        echo .whl file already exists, skipping build.
+    )
+
+    for %%f in (dist\xformers-*.whl) do (
+        pip install dist\%%f
+    )
+
+    python -c "import xformers; print(xformers.__version__)"
+
+    cd ..
+)
+
+
+if not exist "scikit-image" (
+    git clone https://github.com/scikit-image/scikit-image.git
+)
+
+cd scikit-image
+
+powershell -Command "(gc skimage\feature\_texture.pxd) -replace 'cdef int _multiblock_lbp\(', 'cdef int _multiblock_lbp\(\) except +' | Out-File -encoding ASCII skimage\feature\_texture.pxd"
+powershell -Command "(gc skimage\feature\_cascade.pxd) -replace 'cdef int classify\(', 'cdef int classify\(\) except +' | Out-File -encoding ASCII skimage\feature\_cascade.pxd"
+powershell -Command "(gc skimage\feature\_cascade.pxd) -replace 'cdef int __pyx_fuse_0_multiblock_lbp\(', 'cdef int __pyx_fuse_0_multiblock_lbp\(\) except +' | Out-File -encoding ASCII skimage\feature\_cascade.pxd"
+powershell -Command "(gc skimage\feature\_texture.pxd) -replace 'cdef int __pyx_fuse_8integrate\(', 'cdef int __pyx_fuse_8integrate\(\) except +' | Out-File -encoding ASCII skimage\feature\_texture.pxd"
+powershell -Command "(gc skimage\feature\_texture.pxd) -replace 'cdef int __pyx_fuse_9integrate\(', 'cdef int __pyx_fuse_9integrate\(\) except +' | Out-File -encoding ASCII skimage\feature\_texture.pxd"
+
+pip install .
+
+cd ../../
+
+REM call ./webui.bat --skip-python-version-check --share --enable-insecure-extension-access --no-download-sd-model --xformers --no-half-vae %auth%
--- webui-user.sh
+++ webui-user.sh
@@ -1,51 +1,104 @@
 #!/bin/bash
-#########################################################
-# Uncomment and change the variables below to your need:#
-#########################################################
+set -e  # Enable exit on error
 
-# Install directory without trailing slash
-#install_dir="/home/$(whoami)"
-
+export PYTORCH_VERSION=2.3.0
+export TORCHVISION_VERSION=0.18.0
 export TORCH_INDEX_URL="https://download.pytorch.org/whl/cu121"
-export TORCH_COMMAND="pip install torch==2.2.2 torchvision==0.17.2 --extra-index-url https://download.pytorch.org/whl/cu121"
+export TORCH_COMMAND="pip install torch==${PYTORCH_VERSION} torchvision==${TORCHVISION_VERSION} --extra-index-url ${TORCH_INDEX_URL}"
 
-# Name of the subdirectory
-#clone_dir="stable-diffusion-webui"
+export XFORMERS_PACKAGE="xformers"
 
-# Commandline arguments for webui.py, for example: export COMMANDLINE_ARGS="--medvram --opt-split-attention"
-#export COMMANDLINE_ARGS=""
+export GRADIO_USER=""
+export GRADIO_PASS=""
 
-# python3 executable
-#python_cmd="python3"
+# Define the packages to install
+check_apt_packages=("mingw-w64" "llvm")
 
-# git executable
-#export GIT="git"
+# Install PyTorch and Torchvision
+pip install torch==${PYTORCH_VERSION} torchvision==${TORCHVISION_VERSION} --extra-index-url ${TORCH_INDEX_URL}
 
-# python3 venv without trailing slash (defaults to ${install_dir}/${clone_dir}/venv)
-#venv_dir="venv"
+# Check if GRADIO_USER is set
+if [ -z "$GRADIO_USER" ]; then
+    echo "GRADIO_USER is not set."
+    auth=""
+else
+    # Check if GRADIO_PASS is set
+    if [ -z "$GRADIO_PASS" ]; then
+        echo "GRADIO_PASS is not set."
+        auth=""
+    else
+        # Construct the auth variable
+        auth="--gradio-auth=${GRADIO_USER}:${GRADIO_PASS}"
+    fi
+fi
 
-# script to launch to start the app
-#export LAUNCH_SCRIPT="launch.py"
+# Update package list and install apt packages
+sudo apt update
 
-# install command for torch
-#export TORCH_COMMAND="pip install torch==1.12.1+cu113 --extra-index-url https://download.pytorch.org/whl/cu113"
 
-# Requirements file to use for stable-diffusion-webui
-#export REQS_FILE="requirements_versions.txt"
+for package in "${check_apt_packages[@]}"; do
+    # Check if the package is already installed
+    if dpkg -l | grep -q "^ii  $package"; then
+        echo "Package $package is already installed."
+    else
+        echo "Following packages are required: $check_apt_packages"
+        exit 1
+    fi
+done
 
-# Fixed git repos
-#export K_DIFFUSION_PACKAGE=""
-#export GFPGAN_PACKAGE=""
+# Check if wheel is installed
+if ! python -c "import wheel" &> /dev/null; then
+    echo "Installing wheel..."
+    pip install wheel
+fi
 
-# Fixed git commits
-#export STABLE_DIFFUSION_COMMIT_HASH=""
-#export CODEFORMER_COMMIT_HASH=""
-#export BLIP_COMMIT_HASH=""
+# Check if XFormers is already installed
+if ! python -c "import xformers" &> /dev/null; then
+    cd repositories
+    echo "Installing XFormers..."
 
-# Uncomment to enable accelerated launch
-#export ACCELERATE="True"
+    # Clone xformers repository if not already cloned
+    if [ ! -d "xformers" ]; then
+        git clone --recurse-submodules https://github.com/facebookresearch/xformers.git
+    fi
 
-# Uncomment to disable TCMalloc
-#export NO_TCMALLOC="True"
+    cd xformers
 
-###########################################
+    if [ ! -f dist/xformers.whl ]; then
+        echo "Building XFormers..."
+
+        python setup.py sdist bdist_wheel
+        for file in dist/xformers-*.whl; do
+            mv "$file" dist/xformers.whl
+        done
+        for file in dist/xformers-*.tar.gz; do
+            mv "$file" dist/xformers.tar.gz
+        done
+    else
+        echo ".whl file already exists, skipping build."
+    fi
+
+    pip install dist/xformers.whl
+
+    python -c "import xformers"
+
+    cd ..
+
+    # Clone and patch scikit-image
+    git clone https://github.com/scikit-image/scikit-image.git
+    cd scikit-image
+    
+    # Apply patches to add exception declarations
+    sed -i 's/cdef int _multiblock_lbp(...)/cdef int _multiblock_lbp(...) except +/g' skimage/feature/_texture.pxd
+    sed -i 's/cdef int classify(...)/cdef int classify(...) except +/g' skimage/feature/_cascade.pxd
+    sed -i 's/cdef int __pyx_fuse_0_multiblock_lbp(...)/cdef int __pyx_fuse_0_multiblock_lbp(...) except +/g' skimage/feature/_cascade.pxd
+    sed -i 's/cdef int __pyx_fuse_8integrate(...)/cdef int __pyx_fuse_8integrate(...) except +/g' skimage/feature/_texture.pxd
+    sed -i 's/cdef int __pyx_fuse_9integrate(...)/cdef int __pyx_fuse_9integrate(...) except +/g' skimage/feature/_texture.pxd
+
+    python setup.py build_ext --inplace
+    pip install .
+
+    cd ../..
+fi
+
+./webui.sh --skip-python-version-check --share --enable-insecure-extension-access --no-download-sd-model --xformers --no-half-vae $auth
